a86386826be4d51b4d2c03d2b8fd785c67f4e773
Daniel Codreanu <danielcdr2006@gmail.com>
2023-04-01 23:50:27 +0300

    Add project files.

---
+++ exercitiu 2 tema 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyFirstProgram2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Introduceti un numar:");
+            int n = int.Parse(Console.ReadLine());
+            int i;
+
+            int fact = 1;
+            for (i = 1; i <= n; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine(fact);
+
+            Console.ReadLine();
+        }
+    }
+}
